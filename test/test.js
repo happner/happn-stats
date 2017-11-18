fb92c6aa74ff584c60576c8b43a10697d32ea973
nomilous <nomilous@gmail.com>
2017-11-18 20:08:57 +0200

    dont lose count on walking timeout

--- test/test.js
+++ test/test.js
@@ -6,14 +6,9 @@ describe('happn-stats', function () {
 
   before('start server', function (done) {
     this.server = new StatsServer({
-      flushInterval: 1000
+      reportInterval: 1000
     });
     this.server.start().then(done).catch(done);
-  });
-
-  before('start client', function (done) {
-    this.client = new StatsClient({ name: 'xx' });
-    setTimeout(done, 500);
   });
 
   beforeEach(function (done) {
@@ -24,73 +19,122 @@ describe('happn-stats', function () {
     }, 500);
   });
 
-  after('stop client', function (done) {
-    this.client.stop();
-    done();
-  });
-
   after('stop server', function (done) {
     this.server.stop().then(done).catch(done);
   });
 
-  it('reports fragments', function (done) {
-    this.timeout(3000);
-    var _this = this;
-    var fragment = {};
+  context('fragments', function () {
 
-    this.server.on('fragment', function (name, period, metrics) {
-      fragment.name = name;
-      fragment.period = period;
-      fragment.metrics = metrics;
+    before('start client', function (done) {
+      this.client = new StatsClient({ name: 'xx' });
+      setTimeout(done, 100);
     });
 
-    var interval = setInterval(function () {
-      _this.client.increment('counter1');
-      _this.client.gauge('gauge1', 0.5);
-    }, 20);
-
-    setTimeout(function () {
-      clearInterval(interval);
-
-      expect(fragment.name).to.be('xx');
-      expect(fragment.period).to.be(200);
-      expect(fragment.metrics.counters.counter1).to.be.greaterThan(7);
-      var gauge1 = fragment.metrics.gauges.gauge1;
-      expect(gauge1.count).to.be.greaterThan(7);
-      expect(gauge1.total / gauge1.count).to.be(0.5);
-
+    after('stop client', function (done) {
+      this.client.stop();
       done();
-    }, 2000);
+    });
+
+    it('emits fragments', function (done) {
+      this.timeout(3000);
+      var _this = this;
+      var fragment;
+
+      this.server.on('fragment', function (_fragment) {
+        fragment = _fragment;
+      });
+
+      var interval = setInterval(function () {
+        _this.client.increment('counter1');
+        _this.client.gauge('gauge1', 0.5);
+      }, 20);
+
+      setTimeout(function () {
+        clearInterval(interval);
+
+        expect(fragment.name).to.be('xx');
+        expect(fragment.period).to.be.greaterThan(195);
+        expect(fragment.period).to.be.lessThan(210);
+        expect(fragment.metrics.counters.counter1).to.be.greaterThan(7);
+        var gauge1 = fragment.metrics.gauges.gauge1;
+        expect(gauge1.count).to.be.greaterThan(7);
+        expect(gauge1.total / gauge1.count).to.be(0.5);
+
+        done();
+      }, 2000);
+
+    });
 
   });
 
-  it('reports metrics', function (done) {
-    this.timeout(3000);
-    var _this = this;
-    var metrics;
+  context('reports', function () {
 
-    this.server.on('report', function (timestamp, _metrics) {
-      metrics = _metrics;
+    before('start client 1', function (done) {
+      this.client1 = new StatsClient({ name: 'xx' });
+      setTimeout(done, 100);
     });
 
-    var interval = setInterval(function () {
-      _this.client.increment('counter2');
-      _this.client.gauge('gauge2', 0.5);
-    }, 20);
+    before('start client 2', function (done) {
+      this.client2 = new StatsClient({ name: 'xx' });
+      setTimeout(done, 100);
+    });
 
-    setTimeout(function () {
-      clearInterval(interval);
+    before('start client 3', function (done) {
+      this.client3 = new StatsClient({ name: 'xx' });
+      setTimeout(done, 100);
+    });
 
-      // cleared in before hook
-      expect(metrics.counters.counter1).to.be(undefined);
-      expect(metrics.gauges.gauge1).to.be(undefined);
-
-      expect(metrics.counters.counter2).to.be.greaterThan(40);
-      expect(metrics.gauges.gauge2).to.be(0.5);
-
+    after('stop client 1', function (done) {
+      this.client1.stop();
       done();
-    }, 2000);
+    });
 
+    after('stop client 2', function (done) {
+      this.client2.stop();
+      done();
+    });
+
+    after('stop client 3', function (done) {
+      this.client3.stop();
+      done();
+    });
+
+    it('reports metrics', function (done) {
+      this.timeout(3000);
+      var _this = this;
+      var metrics;
+
+      this.server.on('report', function (timestamp, _metrics) {
+        metrics = _metrics;
+      });
+
+      var interval1 = setInterval(function () {
+        _this.client1.increment('counter2');
+        _this.client1.gauge('gauge2', 0.5);
+        _this.client2.increment('counter2');
+        _this.client2.gauge('gauge2', 0.5);
+        _this.client3.increment('counter2');
+        _this.client3.gauge('gauge2', 0.5);
+      }, 20);
+
+
+
+      setTimeout(function () {
+        clearInterval(interval1);
+
+        // cleared in before hook
+        expect(metrics.counters.counter1).to.be(undefined);
+        expect(metrics.gauges.gauge1).to.be(undefined);
+
+        expect(metrics.counters.counter2).to.be.greaterThan(120);
+        expect(metrics.counters.counter2).to.be.lessThan(150);
+        expect(metrics.gauges.gauge2).to.be(0.5);
+
+        done();
+      }, 2000);
+
+
+    });
 
   });
 
